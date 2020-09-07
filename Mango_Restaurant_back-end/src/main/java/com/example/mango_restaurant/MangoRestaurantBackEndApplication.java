9ea35cf16ad77d6a31b89ea2c4b77b1c998f6d0b
Maheshi Purnima <mashipurnima1998@gmail.com>
2020-09-07 12:49:46 +0530

    mashi commit 1

---
+++ Mango_Restaurant_back-end/src/main/java/com/example/mango_restaurant/MangoRestaurantBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.mango_restaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangoRestaurantBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MangoRestaurantBackEndApplication.class, args);
+	}
+
+}
---
+++ Mango_Restaurant_back-end/src/test/java/com/example/mango_restaurant/MangoRestaurantBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mango_restaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MangoRestaurantBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
